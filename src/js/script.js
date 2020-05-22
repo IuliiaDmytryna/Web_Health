9411f36e55ab889223a3ae30fa67ca8e6b818301
Iuliia <iuliiadmytryna@gmail.com>
2020-05-22 19:36:19 +0300

    a mask for phone input has been added

--- src/js/script.js
+++ src/js/script.js
@@ -77,4 +77,5 @@ $(document).ready(function(){
         validateForms('#consultation form');
         validateForms('#order form');
         validateForms("#consulting_form");
+        $('input[name=phone]').mask("+7 (999)999-99-99");
   });
